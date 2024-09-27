ecc412f2aa3b0ab304f13c33182c80430ca99cef
hanwool-mango <hanwool@mangomedical.io>
2024-09-27 19:22:54 +0200

    KAN-3 initialize SQLAlchemy

--- src/app.py
+++ src/app.py
@@ -5,6 +5,8 @@ from .config import load_config
 from .controllers.statistics_controller import stats_blueprint
 from .controllers.interactions_controller import interactions_blueprint
 from .controllers.products_controller import products_blueprint
+from .db import db
+
 
 # Load env vars from .env, for dev environment to work the same as prod.
 # In prod, this line loads nothing as we don't deploy a .env file.
@@ -19,5 +21,13 @@ def create_app(testing=False):
 
     conf = load_config(testing)
     app.config.from_object(conf)
+
+    # Initialize the SQLAlchemy extension with the Flask app
+    db.init_app(app)
+
+    # Reflect the existing tables from the database
+    with app.app_context():
+        metadata = db.metadata
+        metadata.reflect(bind=db.engine)
 
     return app
--- src/config.py
+++ src/config.py
@@ -10,10 +10,16 @@ class Config:
     DB_ADMIN_PASSWORD = "thepassword"
     DB_PORT = 5432
     ENV = "dev"
+    
+    # Build the SQLAlchemy database URI
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestConfig(Config):
     DB_HOST = "business-db"
+
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{Config.DB_USER}:{Config.DB_PASSWORD}@{DB_HOST}:{Config.DB_PORT}/{Config.DB_NAME}"
 
 
 def load_config(testing: bool):
